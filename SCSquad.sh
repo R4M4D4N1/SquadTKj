81f2862b695ad2190072408025dbd363a8d0cdb9
R4M4D4N1 <52308821+R4M4D4N1@users.noreply.github.com>
2019-07-22 23:20:43 +0700

    Add files via upload

---
+++ SCSquad.sh
@@ -0,0 +1,24 @@
+#!/system/xbin/bash
+clear
+sleep 1
+echo -e "\e[32mSMK YP 17 SELOREJO\e[0m";
+sleep 2
+echo -e "\e[35mFGS \"Falcon Squad Gamers\"\e[0m";
+sleep 3
+echo -e "\e[35mSQUAD BY XII TKJ\e[0m";
+sleep 4
+echo -e "\e[34mMr.R4M4 D4N1\e[0m";
+echo -e "\e[32mMrFaril\e[0m";
+echo -e "\e[35mMrImamZ.\e[0m";
+echo -e "\e[36mMrWahyuN\e[0m";
+echo -e "\e[31mMrAlfan\e[0m";
+echo -e "\e[95mMrShodiqulW\e[0m";
+fi
+if
+[  $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo -e "\e[33mKeluar\e[0m";
+sleep 1
+echo -e "\e[32m\"Sampek Ketemu Maneh\"\e[0m";
+sleep 1
+fi
